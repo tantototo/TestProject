e47134dedcda9b5332f2c204a99ed9931438578d
tantototo <k.barkin@yandex.ru>
2021-12-15 12:14:27 +0300

    2

---
+++ TestProjectWeb/Controllers/HistoryController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TestProjectWeb.Data;
+using TestProjectWeb.Models;
+
+namespace TestProjectWeb.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public HistoryController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accounts
+        public async Task<IActionResult> Index()
+        {
+            var appDBContext = _context.History.Include(a => a.Account);
+            return View(await appDBContext.ToListAsync());
+        }
+
+        // GET: Accounts/Details/5
+        public async Task<IActionResult> OpenAccountHistory(int? id)
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null || id == null)
+            {
+                return NotFound();
+            }
+            if (account.Histories == null || account.Histories.Count() == 0)
+            {
+                return NotFound();
+            }
+            var history = account.Histories.OrderByDescending(h => h.OperDate).ToList();
+            return View(nameof(Index), history);
+        }
+
+    }
+}
---
+++ TestProjectWeb/Models/History.cs
@@ -0,0 +1,24 @@
+﻿
+namespace TestProjectWeb.Models
+{
+    public class History
+    {
+        public enum Operations { Put, Withdraw }
+
+        public int Id { get; set; }
+        public Operations Operation { get; set; }
+        public DateTime OperDate { get; set; }
+        public int? Sum { get; set; }
+
+        public int? AccountId { get; set; }
+        public Account? Account { get; set; }
+
+        //public History(Operations oper, int? _sum, Account account)
+        //{
+        //    Operation = oper;
+        //    OperDate = new DateTime();
+        //    Sum = _sum;
+        //    Account = account;
+        //}
+    }
+}
